91fa7692e1e35638e7b70f6402754210d4e42282
Shubham Anand <shubham@radiantsolution.co>
2024-02-19 15:29:23 +0530

    Ui-Changes-Reg-Client: Demo testing

--- registration/registration-client/src/main/java/io/mosip/registration/util/common/ChangeActionHandler.java
+++ registration/registration-client/src/main/java/io/mosip/registration/util/common/ChangeActionHandler.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.Pane;
 public abstract class ChangeActionHandler {
 
     public static final String HASH = "#";
+
     public abstract String getActionClassName();
     public abstract void handle(Pane parentPane, String source, String[] args);
 
